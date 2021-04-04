8338ceb046687acb744acb723b82da4718ec1fd5
frankpenhall <fpenhallegon@broncos.uncfsu.edu>
2021-04-04 18:43:44 -0400

    Screener function added

--- testgraph.py
+++ testgraph.py
@@ -1,8 +1,11 @@
 import yfinance as yf
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import datetime
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
+
+
+"A golden cross is considered a bullish sign; it occurs when the 50-day moving average \
+rises above 200-day moving average. A death cross is considered a bearish sign; it \
+occurs when the 50-day moving average drops below 200-day moving average."
 
 def summary(test):
     symbol = test
@@ -28,4 +31,14 @@ def summary(test):
 
     return name, exchange, sector, industry, ask, bid, _open, prevClose, divYield, beta, forwardPE, vol_10_days, vol, longSummary
 
+def screener_1():
+    URL = ("https://finviz.com/screener.ashx?v=111&f=cap_micro,geo_usa,sh_relvol_o2&o=-change")
+    req = Request(URL, headers={'User-Agent': 'Mozilla/5.0'})
+    webpage = urlopen(req).read()
 
+    soup = BeautifulSoup(webpage, 'html.parser')
+    tickers = soup.find_all('a', attrs={'class': 'screener-link-primary'})
+
+    symbol_list = []
+    for ticker in tickers:
+        symbol_list.append(ticker.text)
