76cd2e343088cfc30352941db2f7e47be30feeba
bobdoe945 <patel.kandarp923@gmail.com>
2021-04-11 19:04:26 -0400

    GUI modified. Added the labels at the top as Frank asked.

--- testgraph.py
+++ testgraph.py
@@ -1,6 +1,8 @@
 import yfinance as yf
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
+import requests
+import pandas as pd
 
 
 def summary(test):
@@ -74,6 +76,30 @@ def screener_2(test):
 
     return symbol_list, names_list, percents_list
 
+
+def topGainers():
+    url = ('https://finance.yahoo.com/gainers')
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    for row in soup.find_all('table')[0].tbody.find_all('tr')[:10]:
+
+        stockSymbol = row.find_all('td')[0].text
+        stockName = row.find_all('td')[1].text
+        stockChange = row.find_all('td')[4].text
+        #        vol = row.find_all('td')[5].text
+        #        threeMonVol = row.find_all('td')[6].text
+
+        stockList = {'Stock Symbol': [stockSymbol],
+                     'Stock Name': [stockName],
+                     'Percentage Change': [stockChange]}
+
+        df = pd.DataFrame(stockList)
+        stock_list = df.values.tolist()
+
+        f = '{:<5} | {:<40} | {:<6}'
+        for i in stock_list:
+            print(f.format(*i))
 
 
 
